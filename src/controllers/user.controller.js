c83a2e2c08a9fabc44c951b90582c92b83df4abc
Prasannakumar55 <prasannakumar55.pk7@gmail.com>
2024-04-23 23:34:11 +0530

    working on user registration'

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,31 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+
 
 const registerUser = asyncHandler( async (req, res) => {
+    // res.status(200).json({
+    //     message: "chai aur code backend"
+    // })
+    const { fullName, email, password } = req.body;
+    console.log('email: ',email);
+    if(
+        [fullName, email, password].some(
+            (value) => value?.trim() === ""
+        )
+    ){
+        throw new ApiError(500, "All Fields are Required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [ { email }, { username }]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "User with Email or Username already Exists")
+    }
     res.status(200).json({
-        message: "chai aur code backend"
+        message: email
     })
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,25 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
 
-// url for below route is http://127.0.0.1:8000/users/register
-router.route("/register").post(registerUser)
+// url for below route is http://127.0.0.1:8000/api/v1/users/register
+// router.route("/register").post(registerUser)
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1,
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser)
 // router.route("/login").post(loginUser)
 
 

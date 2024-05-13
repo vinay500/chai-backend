2e738cf849341f5b184985932ba7f3d4d45bd2f8
vinay500 <vinaymadugula20@gmail.com>
2024-05-13 18:16:57 +0530

    added add and update comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,9 +1,11 @@
-import mongoose from "mongoose"
+import mongoose, { connect } from "mongoose"
 import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import { Video } from "../models/video.model.js"
+import { ownerOrNot } from "../utils/checkOwnerPermission.js"
+
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
@@ -14,11 +16,14 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
-    const { content, videoId } = req.body;
+
+    const { content } = req.body;
     
     if(!content){
         throw new ApiError(400, "Content is Required");
     }
+
+    const { videoId } = req.params;
 
     if(!videoId){
         throw new ApiError(400, "Video ID is Required");
@@ -30,7 +35,7 @@ const addComment = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Invalid Video ID");
     }
 
-    const user = req.user;
+    const user = req?.user;
     
     if(!user){
         throw new ApiError(400, "Login and Try Again");
@@ -39,16 +44,81 @@ const addComment = asyncHandler(async (req, res) => {
     const comment = await Comment.create({
         content,
         video,
-        user
+        owner:user
     })
+
+    return res.status(200).json(
+        new ApiResponse(200, comment, "Tweet Created Successfully")
+    )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    // get comment by id
+    // use Comment.findByIdAndUpdate(set)
+    const { commentId } = req.params;
+
+    const comment = await Comment.findById(commentId);
+
+    const checkOwner = ownerOrNot(comment,req);
+
+    console.log("checkOwner:",checkOwner);
+
+    if(!checkOwner || checkOwner == "Error Occured"){
+        throw new ApiError(400,"You can't Update the Tweet as you are not the Owner");
+    }
+
+    const { content } = req.body;
+
+    const updatedComment = await Comment.findByIdAndUpdate(commentId,
+        {
+            $set:{
+                content: content,
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!updatedComment){
+        throw new ApiError(400, "Can't Update Comment, Try Again");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, updatedComment, "Comment Updated Successfully")
+    )
+
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+
+    const { commentId } = req.params;
+
+    const comment = await Comment.findById(commentId);
+
+    console.log("comment: ",comment);
+
+    const checkOwner = ownerOrNot(comment, req);
+
+    console.log("checkOwner:",checkOwner);
+
+    if(!checkOwner || checkOwner == "Error Occured"){
+        throw new ApiError(400,"You can't Update the Tweet as you are not the Owner");
+    }
+
+    const { content } = req.body;
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId)
+
+    if(!deletedComment){
+        throw new ApiError(400, "Can't Delete Comment, Try Again");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, deletedComment, "Comment Deleted Successfully")
+    )
 })
 
 export {
